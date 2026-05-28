5fa3dad8c45dbfbcc3498d46ab400a481e0c5905
AhmadHoussein <115115970+ahmadhoussein@users.noreply.github.com>
2026-05-28 16:41:30 +0300

    Delete en directory

--- en/account/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    setcookie('Email', ' ', time() - 3600, '/');
-    session_start();
-    session_destroy();
-    header("location: ./../../index.html");
-    die();
-
-?>
--- en/js/for-contact.js
+++
@@ -1,5 +0,0 @@
-function copyTextAndSubmit(){
-  document.getElementById('message').value = document.getElementById('text-message').value;
-  document.getElementById('form').action = "https://formsubmit.co/teddy.khoury73@gmail.com";
-  document.getElementById('submitEmail').style.visibility = "visible";
-}
